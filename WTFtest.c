0f658692583b6e54ec830ce1b12fdf6053fb9ae6
Elijah Ongoco <lejongoco@gmail.com>
2020-05-05 23:00:31 -0400

    added portnum and kill for test

--- WTFtest.c
+++ WTFtest.c
@@ -1,24 +1,44 @@
 #include <stdlib.h> 
 #include <stdio.h> 
 #include <string.h> 
-#include <unistd.h> 
-#include <arpa/inet.h>
-#include <fcntl.h>
-#include <sys/socket.h> 
-#include <netinet/in.h> 
 #include <pthread.h>
-#include <dirent.h>
-#include <errno.h>
-#include <ctype.h>
 
 void* serverRoutine(void* arg);
 void* clientRoutine(void* arg);
 
+struct threadargs{
+    char* port;
+};
+
 int main(int argc, char** argv){
+    //REQUIRES ARGUMENT OF PORT NUMBER
     pthread_t ptid;
     pthread_t ptid2;
 
-    pthread_create(&ptid, NULL, serverRoutine, NULL);
+    if(argc != 2){ //these if statements look for a valid port number as the argument in argv[1]
+        printf("Invalid argument count, need portnum for server\n");
+        exit(EXIT_FAILURE);
+    }
+    else{
+        int length = strlen(argv[1]);
+        int j = 0;
+        for(j = 0; j < length; j++){
+            if(!isdigit(argv[1][j])){
+                printf("Invalid argument, please enter valid port number.\n");
+                exit(EXIT_FAILURE);
+            }
+        }
+        int PORT = atoi(argv[1]);
+        if(PORT < 0 || PORT > 65535){
+            printf("Valid port numbers are between 0 and 65535\n");
+            exit(EXIT_FAILURE);
+        }
+    }
+
+    struct threadargs* toPass = (struct threadargs*)malloc(sizeof(struct threadargs));
+    toPass->port = argv[1];
+
+    pthread_create(&ptid, NULL, serverRoutine, (void*)toPass);
     sleep(1);
     pthread_create(&ptid2, NULL, clientRoutine, NULL);
 
@@ -29,7 +49,11 @@ int main(int argc, char** argv){
 }
 
 void* serverRoutine(void* arg){
-    system("./WTFserver 6969");
+    char port[256] = {0};
+    strcat(port, "./WTFserver ");
+    char hold[256] = {0};
+    strcat(port, ((struct threadargs*)arg)->port);
+    system(port);
 }
 
 void* clientRoutine(void* arg){
@@ -63,6 +87,7 @@ void* clientRoutine(void* arg){
     system("./WTF destroy testFolder");
     //delete the file in order to test checkout and rollback
     system("rm -rf client/testFolder");
-    system("./WTF rollback testFolder 0");
+    system("./WTF rollback testFolder 1");
     system("./WTF checkout testFolder");
+    system("killall WTFserver");
 }
